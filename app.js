b327ee2567aff20d9a7c947a25d89b3b415fefac
PingyuXue <1992x520py@gmail.com>
2018-04-11 19:51:33 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,50 +1,24 @@
-
-// require the express module inside the app.js and create an app
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
+
 var app = express();
 app.use(express.static(path.join(__dirname,"/html")));
 
+app.use(bodyParser.json());
 
 
-
-// http://localhost:7777/index.html 
-
-
-
-// 声明的变量应该在前面
-// 不然  app.post 读取
-// require the body-parse module in app.js
-var bodyParser = require( "body-parser");
-
-app.use( bodyParser.json);
-
-
-//  不知道post 要干嘛！
-// 同时， 需要使用 body-parser module ， post from React client side.
-// ter:   npm install body-parser --save
-
-// create a method called signin:
-//  ？？？ 定义func 的方式？
-app.post( '/signin', function ( req, res){
-    
-    var user_name = req.body.email;
-    var password = req.body.password;
-
-    if ( user_name == 'admin' && password == 'admin'){
-        res.send( 'success');
-    }
-    else{
-        res.send( 'failure');
-    }
+app.post('/signin', function (req, res) {
+  var user_name=req.body.email;
+  var password=req.body.password;
+  if(user_name=='admin' && password=='admin'){
+  	res.send('success');
+  }
+  else{
+  	res.send('Failure');
+  }
 })
 
-
-
-
-
-
- //assign a port number for the application on a port
-app.listen(7777, function(){
-    console.log("started listening on port.",7777);
+app.listen(7777,function(){
+    console.log("Started listening on port", 7777);
 })
